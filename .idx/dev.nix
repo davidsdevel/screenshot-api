0629f6c977b3ffbfee25086c89294246831f1b48
davidsdevel <davidsdevel@gmail.com>
2024-08-05 21:52:47 +0000

    first commit

---
+++ .idx/dev.nix
@@ -0,0 +1,21 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.corepack_20
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "rangav.vscode-thunder-client"
+    ];
+    workspace = {
+    };
+  };
+}
